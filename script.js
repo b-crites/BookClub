e49860140417949f1a61e0b3b50195c8c8fbd13c
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-03-04 11:54:32 -0800

    typo fixes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Link test')
+
+var openLibraryURL='https://openlibrary.org/api/books'
+
+function getApi(){
+fetch(openLibraryURL)
+.then(function(data){
+    console.log(data)
+})
+}
+getApi()
