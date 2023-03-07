11462a5f49081c82f11ffc8796290e149c604398
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-03-06 21:24:43 -0800

    fetch funtion for genre input, id's added to genre input and search button for JS reference

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 console.log('Link test')
-/*
-var openLibraryURL='https://openlibrary.org/api/books'
+var searchButton= document.getElementById('searchButton')
 
-function getApi(){
-fetch(openLibraryURL)
+var openLibraryURL='https://openlibrary.org/subjects/'
+
+function getApi(event){
+    event.preventDefault()
+    var genreInput= document.getElementById('genreInput').value.toLowerCase().split(' ').join('+')
+    console.log(genreInput)
+    
+fetch(openLibraryURL+genreInput+'.json' , {
+    // method: 'no-cors'
+})
+.then(function (response) {
+    return response.json();
+})
 .then(function(data){
     console.log(data)
 })
 }
-getApi()
-*/
+searchButton.addEventListener('click',getApi)
