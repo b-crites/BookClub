70f2ed7ec4367210d90bb5dd8954fc4596a5e560
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-03-09 16:40:39 -0800

    fetch loop to grab more details on from the first fetch

--- script.js
+++ script.js
@@ -1,49 +1,65 @@
 console.log('Link test')
 var searchButton = document.getElementById('searchButton')
 var openLibraryURL = 'https://openlibrary.org'
-var worksArray= []
+var worksArray = []
 var marvelURL = "https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&apikey=ee2ad0bf7d1f2170031816014df5a8bf"
-function getApi(event) {
+function getApi(event) {dult
     event.preventDefault()
-    var genreInput = document.getElementById('genreInput').value.toLowerCase().split(' ').join('+')
+    worksArray= []
+    var genreInput = document.getElementById('genreInput').value.toLowerCase().split(' ').join('_')
     console.log(genreInput)
-    if(genreInput === 'marvel'){
-    fetch(marvelURL, {
-        // method: 'no-cors'
-    })
-        .then(function (response) {
-            return response.json();
+    if (genreInput === 'marvel') {
+        fetch(marvelURL, {
+            // method: 'no-cors'
         })
-        .then(function (data) {
-            console.log(data)
-        })
-    }  
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+            })
+    }
 
-else{
-fetch(openLibraryURL + "/subjects/" + genreInput + '.json'+'?per_page=10', {
-    // method: 'no-cors'
-})
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-        for (var i = 0; i < data.works.length; i++) {
-           worksArray.push(data.works[i])
-        }
-    })
-}
+    else {
+        fetch(openLibraryURL + "/subjects/" + genreInput + '.json' + '?per_page=10', {
+            // mode: 'no-cors'
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+                for (var i = 0; i < data.works.length; i++) {
+                    worksArray.push(data.works[i])
+                }
+                console.log(worksArray)
+            })
+            .then( function (){
+        for (var i = 0; i < worksArray.length; i++) {
+            console.log (worksArray[i].key)
+            fetch(openLibraryURL + '/api/books?bibkeys=OLID:' + worksArray[i].cover_edition_key +'&jscmd=data' + '&format=json',{
+               // mode: 'no-cors'
+            })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data)
+                })
+            }
+        })
+    }
 }
 searchButton.addEventListener('click', getApi)
 
 //A function that picks and displays the selected info in our ul element "title-display"
 //List items need a card style display, Title, Description, and save button element (images if possible)
-function searchDisplay()
+function searchDisplay() { }
 
 //Write a function that saves the name and a link to favorited titles
 //The link should connect to titles page on either open library or the Marvel website
-function saveTitle()
+function saveTitle() { }
 
 //Write a function that displays the save titles and links in our display section "saved-titles"
 //The link should connect to titles page on either open library or the Marvel website
-function saveDisplay()
+function saveDisplay() { }
