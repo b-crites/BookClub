4e8bc98af00f3bc9e5db4da49584d5592fe03cae
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-03-13 21:16:59 -0700

    shuffles fetch results for randomized books selection

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ var openLibraryURL = 'https://openlibrary.org';
 var worksArray = [];
 var saveButton = "";
 var marvelURL = "https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&apikey=ee2ad0bf7d1f2170031816014df5a8bf";
+
 function getApi(event) {
     event.preventDefault();
     clearDisplay();
     worksArray = [];
+   
     var genreInput = document.getElementById('genreInput').value.toLowerCase().split(' ').join('_');
     console.log(genreInput);
     if (genreInput === 'marvel') {
@@ -28,18 +30,28 @@ function getApi(event) {
     }
 
     else {
-        fetch(openLibraryURL + "/subjects/" + genreInput + '.json' + '?per_page=10', {
+        fetch(openLibraryURL + "/subjects/" + genreInput + '.json', {
             // mode: 'no-cors'
         })
             .then(function (response) {
                 return response.json();
             })
+            .then(function (response){
+                let random =Math.random();
+                let randomNumber= Math.floor(random*(response.work_count-5)) +1
+                console.log(randomNumber)
+                fetch(openLibraryURL + "/subjects/" + genreInput + '.json'  + '?limit=5&offset=' + randomNumber)
+
+            .then(function(data){
+                return data.json()
+            })
+
             .then(function (data) {
                 console.log(data);
                 for (var i = 0; i < data.works.length; i++) {
                     worksArray.push(data.works[i])
                 }
-                console.log(worksArray);
+                
             })
             .then(function () {
                 for (var i = 0; i < worksArray.length; i++) {
@@ -57,6 +69,7 @@ function getApi(event) {
                 };
 
             })
+        })
     }
 
 };
