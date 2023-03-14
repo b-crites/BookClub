fa49b3359e571c535483bde6116e1ad40731c6b6
lyskz <lyski.zach@icloud.com>
2023-03-13 21:37:23 -0700

    fixed error

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function saveTitle(data) {
         savedData[1] = savedData[1] + url + ", ";
         localStorage.setItem("savedData", JSON.stringify(savedData));
     };
-    saveDisplay(e);
+    saveDisplay(data);
 };
 
 // In saveDisplay if/else statement to diferentiate between normal data vs marvel data
